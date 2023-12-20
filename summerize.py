a694bd638521d0269be9c1553c17cf4648d61664
Charles McCarthy <charles@fulljourney.ai>
2023-12-19 23:36:32 -0500

    Your commit message here

--- summerize.py
+++ summerize.py
@@ -2,37 +2,50 @@ import replicate
 import os
 from openai import OpenAI
 import argparse
+import random
 
+job = random.randint(1, 100000)
 
 oaiclient = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
 DEFAULT_FILTER = "you are a video summerizer and you will recieve a video transcription from whisper to summerize"
 def get_response(prompt):
-    MAX_CHARS = 50000  # Maximum characters per API call
-    responses = []
+  """
+  Retrieves a response by splitting the prompt into chunks of 50k characters,
+  sending each chunk to an API for completion, and combining the responses
+  into a single string.
 
-    # Split the prompt into chunks of 50k characters
-    prompts = [prompt[i:i+MAX_CHARS] for i in range(0, len(prompt), MAX_CHARS)]
+  Args:
+    prompt (str): The prompt to be sent for completion.
 
-    for prompt in prompts:
-        prompt = "Please summerize the following video which has been transcribed by whisper. Use and tell me important quotes so it feels as if i watched it: \n" + prompt
-        messages = [{"role": "system", "content": DEFAULT_FILTER}] + [{"role": "user", "content": prompt}]
-        completion = oaiclient.chat.completions.create(model="gpt-4-1106-preview", messages=messages)
-        message = completion.choices[0].message
-        responses.append(message.content)
+  Returns:
+    str: The combined response from the API.
 
-    # Combine the responses into a single string
-    response = ' '.join(responses)
-    return response
+  Raises:
+    None
+  """
+  MAX_CHARS = 50000  # Maximum characters per API call
+  responses = []
+
+  # Split the prompt into chunks of 50k characters
+  prompts = [prompt[i:i+MAX_CHARS] for i in range(0, len(prompt), MAX_CHARS)]
+
+  for prompt in prompts:
+    prompt = "Please summerize the following video which has been transcribed by whisper. Use and tell me important quotes so it feels as if i watched it: \n" + prompt
+    messages = [{"role": "system", "content": DEFAULT_FILTER}] + [{"role": "user", "content": prompt}]
+    completion = oaiclient.chat.completions.create(model="gpt-4-1106-preview", messages=messages)
+    message = completion.choices[0].message
+    responses.append(message.content)
+
+  # Combine the responses into a single string
+  response = ' '.join(responses)
+  return response
 parser = argparse.ArgumentParser(description='make a description of a video')
 parser.add_argument('--url', type=str, required=True,
                         help='youtube url')
-parser.add_argument('--job', type=str, required=True,
-                        help='job number')
 args = parser.parse_args()
 url = args.url
-job = args.job
 
 # Run audio through the API
 output = replicate.run(
